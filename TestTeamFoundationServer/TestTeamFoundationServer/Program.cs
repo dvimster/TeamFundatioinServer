866a7398e5f47494e8c4ccc978bc2f67e24e45c2
dvimster <dvimster@gamil.com>
2015-11-30 00:06:08 +0200

    first commit

---
+++ TestTeamFoundationServer/TestTeamFoundationServer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTeamFoundationServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker joker = new Worker {Name = "Joker"};
+            Console.WriteLine(joker.Name);
+        }
+    }
+
+    class Worker
+    {
+        public string Name { get; set; } 
+    }
+}
